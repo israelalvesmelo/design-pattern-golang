9905de58aa011d6d23d43e7913ef900c2b93986e
Israel <israelalvesmelo@hotmail.com>
2023-05-10 22:58:02 -0300

    Learning about build functional

---
+++ builder/functional/functional.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name, position string
+}
+
+type personMod func(*Person)
+type PersonBuilder struct {
+	actions []personMod
+}
+
+func (b *PersonBuilder) Called(name string) *PersonBuilder {
+	b.actions = append(b.actions, func(p *Person) {
+		p.name = name
+	})
+
+	return b
+}
+
+func (b *PersonBuilder) Build() *Person {
+	p := Person{}
+	for _, action := range b.actions {
+		action(&p)
+	}
+
+	return &p
+}
+
+func (b *PersonBuilder) WorkAsA(position string) *PersonBuilder {
+	b.actions = append(b.actions, func(p *Person) {
+		p.position = position
+	})
+
+	return b
+}
+
+func main() {
+	b := PersonBuilder{}
+	p := b.Called("Dmitri").WorkAsA("developer").Build()
+	fmt.Println(*p)
+}
